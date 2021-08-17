b38d3a369fb7b717d431b51d310eb23334f404d5
Jihun <hjihun2315@gmail.com>
2021-08-17 14:38:47 +0900

    feat: change input image pyramid

--- draw_graph.py
+++ draw_graph.py
@@ -5,34 +5,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# results = np.loadtxt('origin.txt')
 results = np.loadtxt('remove_ONet.txt')
 
 # results = sorted(results, key=lambda x: (results[:][0], results[:][1]))
-results = sorted(results, key=operator.itemgetter(0), reverse=True)
+results = np.array(sorted(results, key=operator.itemgetter(0), reverse=True))
 # print(results)
 total_objects = 159424
 
-df = pd.DataFrame(results, columns=['confidence', 'IoU', 'TP', 'FP'])
-print(np.sum(results[0:][2]))
-print(np.array(results)[:, 2])
-print(np.sum(df['TP']))
-print(np.sum(results[0:][3]))
-print(np.sum(df['FP']))
-
-total_count = 0
+item_count = 0
 tp_count = 0
 fp_count = 0
 graph_list = []
 for item in results:
-    total_count += 1
+    item_count += 1
     if item[2] == 1.:
         tp_count += 1
+    else:
+        fp_count += 1
+    graph_list.append([item[0], item_count, tp_count, fp_count])
 
+graph_df = pd.DataFrame(graph_list, columns=['confidence', 'item_count', 'TP_count', 'FP_count'])
 
-# plt.figure(figsize=(40, 5))
-# # sns.lineplot(data=df, x=df['TP']/total_objects, y=df['TP']/(df['TP']+df['FP']))
-# sns.lineplot(data=df, x=df['TP'], y=df['FP'])
-# plt.title('PR Curve(ONet 제거)')
-# plt.xlabel('Recall')
-# plt.ylabel('Precision')
-# plt.show()
+plt.figure(figsize=(40, 6))
+# sns.lineplot(data=df, x=df['TP']/total_objects, y=df['TP']/(df['TP']+df['FP']))
+sns.lineplot(data=graph_df, x=graph_df['TP_count']/total_objects, y=graph_df['TP_count']/graph_df['item_count'])
+# plt.title('PR Curve(Original)')
+plt.title('PR Curve(Remove ONet)')
+plt.xticks(np.arange(0, 1.1, 0.1))
+plt.xlabel('Recall')
+plt.yticks(np.arange(0.0, 1.2, 0.1))
+plt.ylabel('Precision')
+plt.show()
